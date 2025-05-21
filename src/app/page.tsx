8280efc177de4dd82b62f675b7246e74e9cb39e4
Munir Mahmud <munir@softic.ai>
2025-05-21 15:29:27 +0600

    updated new test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,17 @@ export default function Home() {
           </li>
         </ol>
 
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos veniam
+          earum mollitia adipisci architecto perspiciatis aperiam quam! Voluptas
+          ea similique, tempora asperiores odio velit natus?
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero
+          sunt tempore distinctio quis animi doloremque.
+        </p>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
